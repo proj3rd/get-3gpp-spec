188ad42f2e31d9c07b6457f8799ba9d9bd3340f2
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-07-16 22:44:24 +0900

    Parse version from filename

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use suppaftp::list::File;
 use suppaftp::FtpStream;
 mod numbering;
 use numbering::get_series;
+mod versioning;
+use versioning::{Version, parse_version};
 
 #[derive(Debug)]
 struct ParsedFile<'a> {
     name: &'a str,
+    version: Version,
 }
 
 fn main() {
@@ -23,11 +26,20 @@ fn main() {
         .list(Some(&path))
         .expect(format!("Failed to list files in '{path}'").as_str())
         .iter()
-        .map(|entry| {File::from_str(entry).unwrap()})
+        .map(|entry| File::from_str(entry).unwrap())
         .filter(|file| file.name().ends_with(".zip") && file.is_file())
         .collect::<Vec<File>>();
-    let parsed_list = list.iter()
-        .map(|file| ParsedFile { name: file.name()}).collect::<Vec<ParsedFile>>();
+    let parsed_list = list
+        .iter()
+        .map(|file| {
+            let name = file.name();
+            let last_index_of_hyphen = name.rfind("-").unwrap();
+            let last_index_of_dot = name.rfind(".").unwrap();
+            let version =
+                parse_version(&name[last_index_of_hyphen + 1..last_index_of_dot]).unwrap();
+            ParsedFile { name, version }
+        })
+        .collect::<Vec<ParsedFile>>();
     for entry in parsed_list.iter() {
         println!("{:?}", entry);
     }
