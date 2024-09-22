ab22aed71d8d7fbd8104a8d681cc94751a4d7167
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-09-22 22:25:34 +0900

    Extract FTP strategy

---
+++ src/ftp.rs
@@ -0,0 +1,124 @@
+use chrono::{DateTime, FixedOffset, Utc};
+use std::{cmp::Ordering, fmt::Display, fs, io::Write, str::FromStr};
+use suppaftp::{list::File, FtpStream};
+
+use crate::{
+    numbering::get_series,
+    versioning::{parse_version, Version},
+};
+
+#[derive(Debug)]
+struct ParsedFile<'a> {
+    name: &'a str,
+    version: Version,
+    date_time: DateTime<Utc>,
+}
+
+impl Display for ParsedFile<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Name: {}, version: {}, date: {}",
+            self.name,
+            self.version,
+            self.date_time.format("%Y-%m")
+        )
+    }
+}
+
+pub fn get(
+    spec: String,
+    rel: Option<u8>,
+    start_date: Option<DateTime<FixedOffset>>,
+    end_date: Option<DateTime<FixedOffset>>,
+    show_list: bool,
+) {
+    let series = get_series(&spec).unwrap();
+    let path = format!("Specs/archive/{series}_series/{}", spec);
+    let mut ftp_stream =
+        FtpStream::connect("ftp.3gpp.org:21").expect("Failed to connect to ftp.3gpp.org");
+    ftp_stream
+        .login("anonymous", "anonymous")
+        .expect("Failed to login to ftp.3gpp.org");
+
+    let list = ftp_stream
+        .list(Some(&path))
+        .expect(format!("Failed to list files in '{path}'").as_str())
+        .iter()
+        .map(|entry| File::from_str(entry).unwrap())
+        .filter(|file| file.name().ends_with(".zip") && file.is_file())
+        .collect::<Vec<File>>();
+    let mut parsed_list = list
+        .iter()
+        .map(|file| {
+            let name = file.name();
+            let last_index_of_hyphen = name.rfind("-").unwrap();
+            let last_index_of_dot = name.rfind(".").unwrap();
+            let version =
+                parse_version(&name[last_index_of_hyphen + 1..last_index_of_dot]).unwrap();
+            let date_time = DateTime::<Utc>::from(file.modified());
+            ParsedFile {
+                name,
+                version,
+                date_time,
+            }
+        })
+        .filter(|file| {
+            match rel {
+                Some(rel) => {
+                    if rel != file.version.major {
+                        return false;
+                    }
+                }
+                None => {}
+            };
+            match (start_date, end_date) {
+                (Some(start), Some(end)) => {
+                    if start > file.date_time || end < file.date_time {
+                        return false;
+                    }
+                }
+                _ => {}
+            }
+            true
+        })
+        .collect::<Vec<ParsedFile>>();
+    parsed_list.sort_by(|a, b| {
+        let major_ordering = b.version.major.cmp(&a.version.major);
+        if major_ordering != Ordering::Equal {
+            return major_ordering;
+        }
+        let minor_ordering = b.version.minor.cmp(&a.version.minor);
+        if minor_ordering != Ordering::Equal {
+            return minor_ordering;
+        }
+        b.version.editorial.cmp(&a.version.editorial)
+    });
+
+    println!("Found {} files", parsed_list.len());
+    if parsed_list.is_empty() {
+        return;
+    }
+    if show_list {
+        for entry in parsed_list.iter() {
+            println!("{}", entry);
+        }
+    } else {
+        let latest_file = parsed_list.first().unwrap();
+        println!("Downloading {}...", latest_file.name);
+        let data = ftp_stream
+            .retr_as_buffer(
+                format!(
+                    "Specs/archive/{series}_series/{}/{}",
+                    spec, latest_file.name
+                )
+                .as_str(),
+            )
+            .expect("Failed to download");
+        let mut file = fs::File::create(latest_file.name)
+            .expect(format!("Failed to create file {}", latest_file.name).as_str());
+        file.write_all(&data.into_inner()).unwrap();
+        println!("Done");
+    }
+    let _ = ftp_stream.quit().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::{cmp::Ordering, fmt::Display, fs, io::Write, str::FromStr};
-
-use chrono::{DateTime, Months, Utc};
+use chrono::{DateTime, Months};
 use clap::Parser;
-use suppaftp::list::File;
-use suppaftp::FtpStream;
+mod ftp;
 mod numbering;
-use numbering::get_series;
 mod versioning;
-use versioning::{parse_version, Version};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -24,25 +19,6 @@ struct Args {
     /// List all corresponding specs instead of downloading one
     #[arg(short, long)]
     list: bool,
-}
-
-#[derive(Debug)]
-struct ParsedFile<'a> {
-    name: &'a str,
-    version: Version,
-    date_time: DateTime<Utc>,
-}
-
-impl Display for ParsedFile<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Name: {}, version: {}, date: {}",
-            self.name,
-            self.version,
-            self.date_time.format("%Y-%m")
-        )
-    }
 }
 
 fn main() {
@@ -63,93 +39,5 @@ fn main() {
         Some(date) => Some(date.clone().checked_add_months(Months::new(2)).unwrap()),
     };
 
-    let series = get_series(&args.spec).unwrap();
-    let path = format!("Specs/archive/{series}_series/{}", args.spec);
-
-    let mut ftp_stream =
-        FtpStream::connect("ftp.3gpp.org:21").expect("Failed to connect to ftp.3gpp.org");
-    ftp_stream
-        .login("anonymous", "anonymous")
-        .expect("Failed to login to ftp.3gpp.org");
-
-    let list = ftp_stream
-        .list(Some(&path))
-        .expect(format!("Failed to list files in '{path}'").as_str())
-        .iter()
-        .map(|entry| File::from_str(entry).unwrap())
-        .filter(|file| file.name().ends_with(".zip") && file.is_file())
-        .collect::<Vec<File>>();
-    let mut parsed_list = list
-        .iter()
-        .map(|file| {
-            let name = file.name();
-            let last_index_of_hyphen = name.rfind("-").unwrap();
-            let last_index_of_dot = name.rfind(".").unwrap();
-            let version =
-                parse_version(&name[last_index_of_hyphen + 1..last_index_of_dot]).unwrap();
-            let date_time = DateTime::<Utc>::from(file.modified());
-            ParsedFile {
-                name,
-                version,
-                date_time,
-            }
-        })
-        .filter(|file| {
-            match args.rel {
-                Some(rel) => {
-                    if rel != file.version.major {
-                        return false;
-                    }
-                }
-                None => {}
-            };
-            match (start_date, end_date) {
-                (Some(start), Some(end)) => {
-                    if start > file.date_time || end < file.date_time {
-                        return false;
-                    }
-                }
-                _ => {}
-            }
-            true
-        })
-        .collect::<Vec<ParsedFile>>();
-    parsed_list.sort_by(|a, b| {
-        let major_ordering = b.version.major.cmp(&a.version.major);
-        if major_ordering != Ordering::Equal {
-            return major_ordering;
-        }
-        let minor_ordering = b.version.minor.cmp(&a.version.minor);
-        if minor_ordering != Ordering::Equal {
-            return minor_ordering;
-        }
-        b.version.editorial.cmp(&a.version.editorial)
-    });
-
-    println!("Found {} files", parsed_list.len());
-    if parsed_list.is_empty() {
-        return;
-    }
-    if args.list {
-        for entry in parsed_list.iter() {
-            println!("{}", entry);
-        }
-    } else {
-        let latest_file = parsed_list.first().unwrap();
-        println!("Downloading {}...", latest_file.name);
-        let data = ftp_stream
-            .retr_as_buffer(
-                format!(
-                    "Specs/archive/{series}_series/{}/{}",
-                    args.spec, latest_file.name
-                )
-                .as_str(),
-            )
-            .expect("Failed to download");
-        let mut file = fs::File::create(latest_file.name)
-            .expect(format!("Failed to create file {}", latest_file.name).as_str());
-        file.write_all(&data.into_inner()).unwrap();
-        println!("Done");
-    }
-    let _ = ftp_stream.quit().unwrap();
+    ftp::get(args.spec, args.rel, start_date, end_date, args.list);
 }
